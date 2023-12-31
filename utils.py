f7343b2106e3ce787cb0a523da51a759a41bb01d
francis-tg <franc9885@gmail.com>
2023-12-31 14:08:03 +0000

    add comments

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
-import calendar
-import locale
+# Importation des modules nécessaires
+import calendar  # Pour la manipulation des dates et des mois
+import locale  # Pour la gestion des paramètres de localisation
 
-# Définir la locale en français
+# Définition de la locale en français
 locale.setlocale(locale.LC_TIME, "en_EN.UTF-8")
 
 
+# Fonction pour créer un pipeline d'agrégation MongoDB
 def create_aggregation_pipeline(group_by, value_field, calculations):
+    # Étape de groupage pour regrouper les données par Pays, Mois et Année
     group_stage = {
         "$group": {
             "_id": {
@@ -16,6 +19,7 @@ def create_aggregation_pipeline(group_by, value_field, calculations):
         }
     }
 
+    # Étape de projection pour formater les données
     project_stage = {
         "$project": {
             "_id": 0,
@@ -25,6 +29,7 @@ def create_aggregation_pipeline(group_by, value_field, calculations):
         }
     }
 
+    # Boucle pour ajouter les opérations d'agrégation (Somme, Moyenne, Maximum, Minimum)
     for calc in calculations:
         if calc == "Somme":
             group_stage["$group"][calc] = {"$sum": f"${value_field}"}
@@ -39,24 +44,29 @@ def create_aggregation_pipeline(group_by, value_field, calculations):
             group_stage["$group"][calc] = {"$min": f"${value_field}"}
             project_stage["$project"]["Minimum"] = f"${calc}"
 
+    # Construction du pipeline d'agrégation final
     pipeline = [group_stage, project_stage]
     return pipeline
 
 
-def execute_pipeline(collection, pipeline: dict) -> list:  # exécute le le pipeline
+# Fonction pour exécuter un pipeline sur une collection MongoDB
+def execute_pipeline(collection, pipeline: dict) -> list:
     results = collection.aggregate(pipeline)
     return list(results)
 
 
+# Fonction pour formater les données pour l'insertion dans MongoDB
 def groupedData(data: list) -> list:
-    # Map numerical month to month name
+    # Création d'une correspondance numérique -> nom du mois en français
     months = {str(i): calendar.month_name[i] for i in range(1, 13)}
     grouped_data = []
+    # Transformation des données pour les adapter à l'insertion dans la base de données
     for entry in data:
         country = entry["GEO (Labels)"]
-        del entry["GEO (Labels)"]  # Remove the country label from the entry
+        del entry["GEO (Labels)"]  # Suppression du libellé du pays de l'entrée
         for key, value in entry.items():
             year, month = key.split("-")
+            # Conversion du mois numérique en nom du mois en français
             month_name_fr = calendar.month_name[int(month)].capitalize()
             grouped_data.append(
                 {"Pays": country, "Mois": month_name_fr, "Année": year, "Valeur": value}
@@ -64,9 +74,29 @@ def groupedData(data: list) -> list:
     return grouped_data
 
 
+# Fonction pour supprimer les clés non nommées d'un dictionnaire
 def removeUnamedToList(data, keyName="Unnamed") -> list:
     for entry in data:
+        # Récupération des clés à supprimer basées sur le nom donné
         keys_to_remove = [key for key in entry.keys() if keyName in key]
+        # Suppression des clés non nommées du dictionnaire
         for key in keys_to_remove:
             del entry[key]
     return data
+
+
+""" 
+Importation des modules : Importe les modules nécessaires pour gérer les dates, les mois et les paramètres de localisation.
+
+Définition de la locale en français : Configure la locale pour afficher les mois en français.
+
+Fonction create_aggregation_pipeline : Crée un pipeline d'agrégation MongoDB en fonction des spécifications de groupage, du champ de valeur et des calculs à réaliser.
+
+Fonction execute_pipeline : Exécute un pipeline sur une collection MongoDB et retourne les résultats.
+
+Fonction groupedData : Transforme les données en un format adapté pour l'insertion dans une base de données MongoDB, en convertissant les mois numériques en noms de mois en français et en organisant les données par pays, mois et année.
+
+Fonction removeUnamedToList : Supprime les clés non nommées d'un dictionnaire, généralement utilisée pour nettoyer les données avant l'insertion dans une base de données.
+
+Ces fonctions sont conçues pour faciliter la préparation des données avant leur insertion dans une base de données MongoDB, en les adaptant à la structure attendue pour les opérations d'agrégation et d'insertion.
+"""
